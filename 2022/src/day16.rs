be0843ea2643575ec700435d61b0e726d013c74f
Alex Thill <thial058@gmail.com>
2022-12-18 16:42:50 +0100

    improved day 16 by 3 orders of magnitude

--- 2022/src/day16.rs
+++ 2022/src/day16.rs
@@ -6,15 +6,14 @@ pub const SOLUTION: Solution = Solution::U32((1820, 2602));
 #[derive(Debug)]
 struct Valve {
     id: u32,
-    flow: u32,
-    leads: Vec<usize>,
+    leads: Vec<u32>,
 }
 
 pub fn solve(input: String) -> Solution {
     let mut valves = Vec::new();
     let mut nonzero_valves = Vec::new();
     let mut iter = input.bytes();
-    let mut start = usize::MAX;
+    let mut start = u32::MAX;
     
     loop {
         if iter.nth("Valve ".len() - 1).is_none() {
@@ -31,7 +30,7 @@ pub fn solve(input: String) -> Solution {
         let mut leads = Vec::new();
         loop {
             let id = iter.next().unwrap() as u32 * 256 + iter.next().unwrap() as u32;
-            leads.push(id as usize);
+            leads.push(id);
             
             if iter.next().unwrap() == b'\n' {
                 break;
@@ -39,32 +38,35 @@ pub fn solve(input: String) -> Solution {
             iter.next().unwrap();
         }
         
-        valves.push(Valve { id, flow, leads });
         if flow > 0 {
-            nonzero_valves.push(valves.len() - 1);
+            nonzero_valves.push((valves.len() as u32, flow));
         }
         if id == b'A' as u32 * 257 {
-            start = valves.len() - 1;
+            start = valves.len() as u32;
         }
+        valves.push(Valve { id, leads });
     }
     
-    assert_ne!(start, usize::MAX, "Did not find start valve named AA");
+    assert_ne!(start, u32::MAX, "Did not find start valve named AA");
     
     for i in 0..valves.len() {
         for j in 0..valves[i].leads.len() {
-            let id = valves[i].leads[j] as u32;
+            let id = valves[i].leads[j];
             let pos = valves.iter().position(|v| v.id == id).unwrap();
-            valves[i].leads[j] = pos;
+            valves[i].leads[j] = pos as u32;
         }
     }
     
-    let mut dist = (0..valves.len()).map(|_| { let mut vec = Vec::new(); vec.resize(valves.len(), u32::MAX / 2); vec }).collect::<Vec<_>>();
-    for (from_idx, valve) in valves.iter().enumerate() {
-        for &to_idx in valve.leads.iter() {
-            dist[from_idx][to_idx] = 1;
+    // this is the Floyd-Warshall algorithm to calculate distances between all valve pairs
+    let mut dist = (0..valves.len()).map(|i| {
+        let mut vec = Vec::new();
+        vec.resize(valves.len(), u8::MAX / 2);
+        for &to_idx in valves[i].leads.iter() {
+            vec[to_idx as usize] = 1;
         }
-        dist[from_idx][from_idx] = 0;
-    }
+        vec[i] = 0;
+        vec.into_boxed_slice()
+    }).collect::<Box<_>>();
     for k in 0..valves.len() {
         for i in 0..valves.len() {
             for j in 0..valves.len() {
@@ -75,74 +77,102 @@ pub fn solve(input: String) -> Solution {
         }
     }
     
-    //let order = [3, 1, 9, 7, 4, 2];
-    let presure1 = calc1::<30>(&valves, &dist, &mut nonzero_valves, start, 0, 0);
-    let presure2 = calc2(&valves, &dist, &mut nonzero_valves, start, start, 0, 0);
+    // using a decent start estimation of the maximum released pressure saves calculations
+    // visit the valves in decending flow order as estimation
+    nonzero_valves.sort_unstable_by_key(|&(_, flow)| flow);
+    let mut curr = 0;
+    let mut flow = 0;
+    let mut time = 30;
+    let mut pressure = 0;
+    for &(to_idx, to_flow) in nonzero_valves.iter().rev() {
+        let elapsed = dist[curr as usize][to_idx as usize] as u32 + 1;
+        if elapsed < time {
+            pressure += elapsed * flow;
+            flow += to_flow;
+            curr = to_idx;
+            time -= elapsed;
+        } else {
+            pressure += time * flow;
+            break;
+        }
+    }
     
-    (presure1, presure2).into()
+    let pressure1 = calc1(&dist, &mut nonzero_valves, start, 0, 30, 0, pressure);
+    let pressure2 = calc2(&dist, &mut nonzero_valves, start, start, 0, 26, 0, pressure1);
+    
+    (pressure1, pressure2).into()
 }
 
-fn calc1<const TIME: u32>(valves: &[Valve], dist: &[Vec<u32>], to_open: &mut Vec<usize>, curr: usize, flow: u32, elapsed: u32) -> u32 {
-    debug_assert!(elapsed <= TIME);
-    
-    if to_open.is_empty() {
-        (TIME - elapsed) * flow
-    } else {
+fn calc1(dist: &[Box<[u8]>], to_open: &mut [(u32, u32)], curr: u32, flow: u32, time: u32, pressure: u32, best: u32) -> u32 {
+    if to_open.len() > 1 {
+        let estimate = to_open.iter().map(|&(idx, flow)| flow * time.saturating_sub(dist[curr as usize][idx as usize] as u32 + 1)).sum::<u32>();
+        if pressure + time * flow + estimate <= best {
+            return 0;
+        }
+        
         let last_idx = to_open.len() - 1;
-        let mut max = 0;
+        let mut max = best.max(pressure + time * flow);
         for i in 0..to_open.len() {
-            let to = to_open[i];
-            let time = dist[curr][to] + 1;
-            let new_elapsed = elapsed + time;
+            let (to_idx, to_flow) = to_open[i];
+            let elapsed = dist[curr as usize][to_idx as usize] as u32 + 1;
             
-            let added = if new_elapsed <= TIME {
-                to_open.swap_remove(i);
-                let new_flow = flow + valves[to].flow;
-                let added = time * flow + calc1::<TIME>(valves, dist, to_open, to, new_flow, new_elapsed);
-                to_open.push(to);
+            if elapsed < time {
                 to_open.swap(i, last_idx);
-                added
-            } else {
-                (TIME - elapsed) * flow
-            };
-            
-            max = max.max(added);
+                let new_flow = flow + to_flow;
+                let new_pressure = pressure + elapsed * flow;
+                let val = calc1(dist, &mut to_open[..last_idx], to_idx, new_flow, time - elapsed, new_pressure, max);
+                max = max.max(val);
+                to_open.swap(i, last_idx);
+            }
         }
         
         max
+    } else {
+        let (to_idx, to_flow) = to_open[0];
+        let elapsed = dist[curr as usize][to_idx as usize] as u32 + 1;
+        
+        if elapsed < time {
+            let new_flow = flow + to_flow;
+            pressure + elapsed * flow + (time - elapsed) * new_flow
+        } else {
+            pressure + time * flow
+        }
     }
 }
 
-fn calc2(valves: &[Valve], dist: &[Vec<u32>], to_open: &mut Vec<usize>, start: usize, curr: usize, flow: u32, elapsed: u32) -> u32 {
-    debug_assert!(elapsed <= 26);
-    
-    if to_open.is_empty() {
-        (26 - elapsed) * flow
-    } else {
-        let last_idx = to_open.len() - 1;
-        let mut max = 0;
-        for i in 0..to_open.len() {
-            let to = to_open[i];
-            let time = dist[curr][to] + 1;
-            let new_elapsed = elapsed + time;
-            
-            let added = if new_elapsed <= 26 {
-                to_open.swap_remove(i);
-                let new_flow = flow + valves[to].flow;
-                let added = time * flow + calc2(valves, dist, to_open, start, to, new_flow, new_elapsed);
-                to_open.push(to);
-                to_open.swap(i, last_idx);
-                added
-            } else {
-                (26 - elapsed) * flow
-            };
-            
-            max = max.max(added);
+fn calc2(dist: &[Box<[u8]>], to_open: &mut [(u32, u32)], start: u32, curr: u32, flow: u32, time: u32, pressure: u32, best: u32) -> u32 {
+    if to_open.len() > 1 {
+        let estimate = to_open.iter().map(|&(idx, flow)| flow * time.saturating_sub(dist[curr as usize][idx as usize] as u32 + 1)).sum::<u32>();
+        // elephant can release at most as much pressure as self
+        if 2 * (pressure + time * flow + estimate) <= best {
+            return 0;
         }
         
-        let val = (26 - elapsed) * flow + calc1::<26>(valves, dist, to_open, start, 0, 0);
+        let last_idx = to_open.len() - 1;
+        let mut max = best.max(pressure + time * flow);
+        for i in 0..to_open.len() {
+            let (to_idx, to_flow) = to_open[i];
+            let elapsed = dist[curr as usize][to_idx as usize] as u32 + 1;
+            
+            if elapsed < time {
+                to_open.swap(i, last_idx);
+                let new_flow = flow + to_flow;
+                let new_pressure = pressure + elapsed * flow;
+                let val = calc2(dist, &mut to_open[..last_idx], start, to_idx, new_flow, time - elapsed, new_pressure, max);
+                max = max.max(val);
+                to_open.swap(i, last_idx);
+            }
+        }
+        
+        let val = calc1(dist, to_open, start, 0, 26, pressure + time * flow, max);
         max = max.max(val);
         
         max
+    } else {
+        // let the elephant open the last valve
+        let (to_idx, to_flow) = to_open[0];
+        let elapsed = dist[start as usize][to_idx as usize] as u32 + 1;
+        assert!(elapsed <= 26);
+        pressure + time * flow + (26 - elapsed) * to_flow
     }
 }
