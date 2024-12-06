87c4c8e5eaeda87553934023414d034b90183e09
alexthill <thial058@gmail.com>
2024-12-06 20:00:17 +0100

    impoved main and added example inputs

--- 2024/src/main.py
+++ 2024/src/main.py
@@ -10,32 +10,40 @@ from day05 import day05
 from day06 import day06
 
 def main():
-    if len(sys.argv) != 2:
-        print(f"Usage: {sys.argv[0]} [day]")
+    if len(sys.argv) < 2:
+        print(f"Usage: {sys.argv[0]} [day] [flags]")
+        print(f"list of flags:")
+        print(f"-e\t\trun example input")
         return
 
+    flags = {}
+    for flag in sys.argv[2:]:
+        flags[flag] = True
+
     day = int(sys.argv[1])
-    file_prefix = "../inputs/"
+    zero = "0" if day < 10 else ""
+    example = "_example" if "-e" in flags else ""
+    file_name = f"../inputs/day{zero}{day}{example}.txt"
+    try:
+        file = open(file_name, "r")
+    except:
+        print(f"failed to open file '{file_name}'")
+        return
+
     print(f"solving day {day} ...")
     match day:
         case 1:
-            with open(file_prefix + "day01.txt") as file:
-                part1, part2 = day01(file)
+            part1, part2 = day01(file)
         case 2:
-            with open(file_prefix + "day02.txt") as file:
-                part1, part2 = day02(file)
+            part1, part2 = day02(file)
         case 3:
-            with open(file_prefix + "day03.txt") as file:
-                part1, part2 = day03(file)
+            part1, part2 = day03(file)
         case 4:
-            with open(file_prefix + "day04.txt") as file:
-                part1, part2 = day04(file)
+            part1, part2 = day04(file)
         case 5:
-            with open(file_prefix + "day05.txt") as file:
-                part1, part2 = day05(file)
+            part1, part2 = day05(file)
         case 6:
-            with open(file_prefix + "day06.txt") as file:
-                part1, part2 = day06(file)
+            part1, part2 = day06(file)
         case _:
             print(f"no solution for day {day}")
             return
