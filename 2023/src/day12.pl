7ac999096c8d09ec7f65b7b70c734c1391f020c1
alexthill <thial058@gmail.com>
2025-02-01 18:39:56 +0100

    2023 day12

--- 2023/src/day12.pl
+++ 2023/src/day12.pl
@@ -1,11 +1,11 @@
 :- ensure_loaded(utils).
 
-main :- 
+main :-
     solve(P1, P2), !,
     writeln(P1), writeln(P2),
     solutions(P1, P2).
 
-solutions(21, 525152).
+solutions(7402, 3384337640277).
 
 solve(Sum, Sum2) :-
     read_file_to_lines_string('../inputs/day12.txt', Lines),
@@ -18,7 +18,8 @@ map_line(Line, N) :-
     split_string(Line, " ", "", LineParts),
     maplist(string_codes, LineParts, [P1,P2]),
     all_nums(P2, Nums),
-    blubb(P1, Nums, N).
+    empty_assoc(C),
+    arrange(C, _, P1, Nums, N).
 
 map_line2(Line, N) :-
     split_string(Line, " ", "", LineParts),
@@ -26,28 +27,21 @@ map_line2(Line, N) :-
     all_nums(P2, Nums),
     append([P1,`?`,P1,`?`,P1,`?`,P1,`?`,P1], P1Repeat),
     append([Nums,Nums,Nums,Nums,Nums], NumsRepeat),
-    blubb(P1Repeat, NumsRepeat, N), writeln(N).
+    empty_assoc(C),
+    arrange(C, _, P1Repeat, NumsRepeat, N).
 
-arrange([], [], 1).
-arrange([0'#|XS], [R|RS], N) :- succ(R0, R), arrange(XS, R0, RS, N).
-arrange([0'?|XS], RS, N) :- arrange(XS, RS, N1), arrange([0'#|XS], RS, N2), !, N is N1 + N2.
-arrange(_, _, 0).
+arrange(C, C, [], [], 1).
+arrange(C, C, XS, RS, N) :- get_assoc(XS-RS, C, N).
+arrange(CI, CO, [0'.|XS], RS, N) :- arrange(CI, CO, XS, RS, N).
+arrange(CI, CO, [0'#|XS], [R|RS], N) :- succ(R0, R), arrange2(CI, CO, XS, R0, RS, N).
+arrange(CI, CO1, [0'?|XS], RS, N) :-
+    (arrange(CI, CI1, XS, RS, N1); N1 is 0, CI1 = CI), !, % these cuts improve performence for some reason
+    (arrange(CI1, CO, [0'#|XS], RS, N2); N2 is 0, CO = CI1), !,
+    N is N1 + N2,
+    put_assoc([0'?|XS]-RS, CO, N, CO1).
 
-arrange([], 0, [], 1).
-arrange([0'?|XS], 0, RS, N) :- arrange(XS, RS, N).
-arrange([0'#|XS], R, RS, N) :- succ(R0, R), arrange(XS, R0, RS, N).
-arrange([0'?|XS], R, RS, N) :- succ(R0, R), arrange(XS, R0, RS, N).
-arrange(_, _, _, 0).
-
-blubb(XS, RS, N) :- string_codes(Str, XS), split_string(Str, ".", ".", Strs), maplist(string_codes, Strs, YS), blubber(YS, RS, N).
-
-blubber([], [], 1).
-blubber([], _, 0).
-blubber(_, [], 0).
-blubber(XS, RS, N) :- bagof([A,B], append(A, B, RS), ABS), maplist(blubbert(XS), ABS, NS), sum_list(NS, N).
-
-blubbert([X|_], [[],_], 0) :- member(0'#, X).
-blubbert([X|XS], [[1],R2], N) :- maplist(=(0'?), X), length(X, N1), blubber(XS, R2, N2), !, N is N1 * N2.
-blubbert([X|XS], [R1,R2], N) :- arrange(X, R1, N1), (N1 =:= 0 -> N is 0 ; blubber(XS, R2, N2), !, N is N1 * N2).
-
-non_empty_list_append([A|AS], BS, CS) :- append([A|AS], BS, CS).
+arrange2(C, C, [], 0, [], 1).
+arrange2(CI, CO, [0'.|XS], 0, RS, N) :- arrange(CI, CO, XS, RS, N).
+arrange2(CI, CO, [0'?|XS], 0, RS, N) :- arrange(CI, CO, XS, RS, N).
+arrange2(CI, CO, [0'#|XS], R, RS, N) :- succ(R0, R), arrange2(CI, CO, XS, R0, RS, N).
+arrange2(CI, CO, [0'?|XS], R, RS, N) :- succ(R0, R), arrange2(CI, CO, XS, R0, RS, N).
