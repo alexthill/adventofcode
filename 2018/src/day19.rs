53446b09497421b8b47058a0bb7042bd453089a6
alexthill <thial058@gmail.com>
2025-06-06 12:38:41 +0200

    2018 day21

--- 2018/src/day19.rs
+++ 2018/src/day19.rs
@@ -1,6 +1,64 @@
 use aoc_lib_rust::{Day, Example, Solution};
 use aoc_lib_rust::{next, next_parse};
 
+// Registers only need to be u32 for this day but u64 for day21.
+// It would run this day faster with u32 but it is already fast
+// and don't want to do generic stuff.
+pub fn parse_instructions(input: &str) -> (usize, Vec<(u8, [u64; 3])>) {
+    let mut lines = input.lines();
+    let (_, n) = next!(lines).split_once(' ').unwrap();
+    let ip = n.parse::<usize>().unwrap();
+    let instructions = lines.map(|line| {
+        let mut parts = line.split(' ');
+        let code = match next!(parts) {
+            "addr" => 0,
+            "addi" => 1,
+            "mulr" => 2,
+            "muli" => 3,
+            "banr" => 4,
+            "bani" => 5,
+            "borr" => 6,
+            "bori" => 7,
+            "setr" => 8,
+            "seti" => 9,
+            "gtir" => 10,
+            "gtri" => 11,
+            "gtrr" => 12,
+            "eqir" => 13,
+            "eqri" => 14,
+            "eqrr" => 15,
+            _ => unreachable!(),
+        };
+        let args = [next_parse!(parts, u64), next_parse!(parts, u64), next_parse!(parts, u64)];
+        (code, args)
+    }).collect::<Vec<_>>();
+    (ip, instructions)
+}
+
+pub fn exec_op(code: u8, args: [u64; 3], mut registers: [u64; 6]) -> [u64; 6] {
+    let [a, b, c] = args.map(|arg| arg as usize);
+    match code {
+        0 => registers[c] = registers[a] + registers[b],
+        1 => registers[c] = registers[a] + args[1],
+        2 => registers[c] = registers[a] * registers[b],
+        3 => registers[c] = registers[a] * args[1],
+        4 => registers[c] = registers[a] & registers[b],
+        5 => registers[c] = registers[a] & args[1],
+        6 => registers[c] = registers[a] | registers[b],
+        7 => registers[c] = registers[a] | args[1],
+        8 => registers[c] = registers[a],
+        9 => registers[c] = args[0],
+        10 => registers[c] = (args[0] > registers[b]) as _,
+        11 => registers[c] = (registers[a] > args[1]) as _,
+        12 => registers[c] = (registers[a] > registers[b]) as _,
+        13 => registers[c] = (args[0] == registers[b]) as _,
+        14 => registers[c] = (registers[a] == args[1]) as _,
+        15 => registers[c] = (registers[a] == registers[b]) as _,
+        _ => unreachable!(),
+    }
+    registers
+}
+
 pub struct Day19;
 
 impl Day for Day19 {
@@ -9,59 +67,9 @@ impl Day for Day19 {
     const PART2: Solution = Solution::U32(27024480);
 
     fn solve(input: &str) -> [Solution; 2] {
-        let mut lines = input.lines();
-        let (_, n) = next!(lines).split_once(' ').unwrap();
-        let ip = n.parse::<usize>().unwrap();
-        let instructions = lines.map(|line| {
-            let mut parts = line.split(' ');
-            let code = match next!(parts) {
-                "addr" => 0,
-                "addi" => 1,
-                "mulr" => 2,
-                "muli" => 3,
-                "banr" => 4,
-                "bani" => 5,
-                "borr" => 6,
-                "bori" => 7,
-                "setr" => 8,
-                "seti" => 9,
-                "gtir" => 10,
-                "gtri" => 11,
-                "gtrr" => 12,
-                "eqir" => 13,
-                "eqri" => 14,
-                "eqrr" => 15,
-                _ => unreachable!(),
-            };
-            let args = [next_parse!(parts, u32), next_parse!(parts, u32), next_parse!(parts, u32)];
-            (code, args)
-        }).collect::<Vec<_>>();
+        let (ip, instructions) = parse_instructions(input);
 
-        fn exec_op(code: u8, args: [u32; 3], mut registers: [u32; 6]) -> [u32; 6] {
-            let [a, b, c] = args.map(|arg| arg as usize);
-            match code {
-                0 => registers[c] = registers[a] + registers[b],
-                1 => registers[c] = registers[a] + args[1],
-                2 => registers[c] = registers[a] * registers[b],
-                3 => registers[c] = registers[a] * args[1],
-                4 => registers[c] = registers[a] & registers[b],
-                5 => registers[c] = registers[a] & args[1],
-                6 => registers[c] = registers[a] | registers[b],
-                7 => registers[c] = registers[a] | args[1],
-                8 => registers[c] = registers[a],
-                9 => registers[c] = args[0],
-                10 => registers[c] = (args[0] > registers[b]) as _,
-                11 => registers[c] = (registers[a] > args[1]) as _,
-                12 => registers[c] = (registers[a] > registers[b]) as _,
-                13 => registers[c] = (args[0] == registers[b]) as _,
-                14 => registers[c] = (registers[a] == args[1]) as _,
-                15 => registers[c] = (registers[a] == registers[b]) as _,
-                _ => unreachable!(),
-            }
-            registers
-        }
-
-        fn get_num(instructions: &[(u8, [u32; 3])], ip: usize, mut registers: [u32; 6]) -> u32 {
+        fn get_num(instructions: &[(u8, [u64; 3])], ip: usize, mut registers: [u64; 6]) -> u64 {
             let mut old_max = 0;
             let mut old_max_age = 0;
             while let Some((code, args)) = instructions.get(registers[ip] as usize) {
@@ -80,11 +88,11 @@ impl Day for Day19 {
             unreachable!()
         }
 
-        fn sum_divisors(num: u32) -> u32 {
-            let to = (num as f32).sqrt().ceil() as u32;
+        fn sum_divisors(num: u64) -> u64 {
+            let to = (num as f32).sqrt().ceil() as u64;
             (1..to).filter_map(|n| {
                 if num % n == 0 { Some(n + num / n) } else { None }
-            }).sum::<u32>() + if to * to == num { to } else { 0 }
+            }).sum::<u64>() + if to * to == num { to } else { 0 }
         }
 
         let num = get_num(&instructions, ip, [0; 6]);
@@ -93,7 +101,7 @@ impl Day for Day19 {
         let num = get_num(&instructions, ip, [1, 0, 0, 0, 0, 0]);
         let sol2 = sum_divisors(num);
 
-        [Solution::U32(sol1), Solution::U32(sol2)]
+        [Solution::U32(sol1 as _), Solution::U32(sol2 as _)]
     }
 
     const EXAMPLES: &'static [Example] = &[
--- 2018/src/day21.rs
+++ 2018/src/day21.rs
@@ -1,20 +1,46 @@
-use aoc_lib_rust::{Day, Example, Solution};
+use aoc_lib_rust::{Day, Solution};
+use super::day19::{exec_op, parse_instructions};
 
 pub struct Day21;
 
 impl Day for Day21 {
 
-    const PART1: Solution = Solution::None;
-    const PART2: Solution = Solution::None;
+    const PART1: Solution = Solution::U32(15690445);
+    const PART2: Solution = Solution::U32(936387);
 
-    fn solve(_input: &str) -> [Solution; 2] {
-        [Solution::None, Solution::None]
+    fn solve(input: &str) -> [Solution; 2] {
+        let (ip, instructions) = parse_instructions(input);
+
+        let mut registers = [0; 6];
+        let sol1 = loop {
+            let Some((code, args)) = instructions.get(registers[ip] as usize) else {
+                panic!("ran out of instructions before finding solution");
+            };
+            if *code == 15 { // eqrr
+                break registers[args[0] as usize];
+            }
+            registers = exec_op(*code as u8, *args, registers);
+            registers[ip] += 1;
+        };
+
+        let mut cache = vec![false; 16777215];
+        let mut last_value = 0;
+        let sol2 = loop {
+            let Some((code, args)) = instructions.get(registers[ip] as usize) else {
+                panic!("ran out of instructions before finding solution");
+            };
+            if *code == 15 { // eqrr
+                let value = registers[args[0] as usize];
+                if cache[value as usize] {
+                    break last_value;
+                }
+                cache[value as usize] = true;
+                last_value = value;
+            }
+            registers = exec_op(*code as u8, *args, registers);
+            registers[ip] += 1;
+        };
+
+        [Solution::U64(sol1), Solution::U64(sol2)]
     }
-
-    const EXAMPLES: &'static [Example] = &[
-        Example {
-            solution: [Solution::None, Solution::None],
-            input: "",
-        },
-    ];
 }
