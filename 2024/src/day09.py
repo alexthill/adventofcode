2c2c6b74025dcf56a7f87bfee98e7267c9c78cc7
alexthill <thial058@gmail.com>
2025-08-21 11:08:47 +0200

    2024 day09 speed improvement

--- 2024/src/day09.py
+++ 2024/src/day09.py
@@ -29,27 +29,26 @@ def day09(file):
         disk[i] = disk[j]
         disk[j] = -1
 
-    sol1 = 0
-    for k in range(0, len(disk)):
-        if disk[k] != -1:
-            sol1 += k * disk[k]
+    sol1 = sum(k * v for k, v in enumerate(disk) if v != -1)
 
     disk = disk2
+    next_free_pos_by_size = [0 for _ in range(10)]
     for full_pos, full_size, full_id in reversed(full_spans):
-        for i, (free_pos, free_size) in enumerate(free_spans):
+        if full_pos < next_free_pos_by_size[1]:
+            break
+        for i in range(next_free_pos_by_size[full_size], len(free_spans)):
+            free_pos, free_size = free_spans[i]
             if free_pos > full_pos:
-                continue
+                break;
             if free_size >= full_size:
                 free_spans[i] = (free_pos + full_size, free_size - full_size)
+                next_free_pos_by_size[full_size] = i
                 for j in range(0, full_size):
                     disk[free_pos + j] = full_id
                 for j in range(0, full_size):
                     disk[full_pos + j] = -1
                 break
 
-    sol2 = 0
-    for k in range(0, len(disk)):
-        if disk[k] != -1:
-            sol2 += k * disk[k]
+    sol2 = sum(k * v for k, v in enumerate(disk) if v != -1)
 
     return (sol1, sol2)
