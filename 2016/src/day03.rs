edf6f17f0c93dcb0c9b25f09480b245e1edd750d
alexthill <thial058@gmail.com>
2025-10-14 11:02:55 +0200

    2016 day03

--- 2016/src/day03.rs
+++ 2016/src/day03.rs
@@ -1,20 +1,44 @@
-use aoc_lib_rust::{Day, Example, Solution};
+use aoc_lib_rust::{next_parse, Day, Example, Solution};
 
 pub struct Day03;
 
 impl Day for Day03 {
 
-    const PART1: Solution = Solution::None;
-    const PART2: Solution = Solution::None;
+    const PART1: Solution = Solution::U32(993);
+    const PART2: Solution = Solution::U32(1849);
 
-    fn solve(_input: &str) -> [Solution; 2] {
-        [Solution::None, Solution::None]
+    fn solve(input: &str) -> [Solution; 2] {
+        let sol1 = input.lines().filter(|line| {
+            let mut parts = line.split(' ').filter(|part| !part.is_empty());
+            let a = next_parse!(parts, u32);
+            let b = next_parse!(parts, u32);
+            let c = next_parse!(parts, u32);
+            a + b > c && a + c > b && b + c > a
+        }).count();
+
+        let sol2 = input.lines().collect::<Vec<&str>>().chunks(3).map(|lines| {
+            let nums = [0, 1, 2].map(|n| {
+                let mut parts = lines[n].split(' ').filter(|part| !part.is_empty());
+                [next_parse!(parts, u32), next_parse!(parts, u32), next_parse!(parts, u32)]
+
+            });
+            (0..3).filter(|&n| {
+                let a = nums[0][n];
+                let b = nums[1][n];
+                let c = nums[2][n];
+                a + b > c && a + c > b && b + c > a
+            }).count() as u32
+        }).sum();
+
+        [Solution::U32(sol1 as _), Solution::U32(sol2)]
     }
 
     const EXAMPLES: &'static [Example] = &[
         Example {
-            solution: [Solution::None, Solution::None],
-            input: "",
+            solution: [Solution::U32(1), Solution::None],
+            input: " 5 10 25
+10  3  3
+ 3  4  5",
         },
     ];
 }
