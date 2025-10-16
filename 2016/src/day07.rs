ffe3612efece1e8a2a9239e767428351c78fc1bf
alexthill <thial058@gmail.com>
2025-10-16 08:54:12 +0200

    2016 day07

--- 2016/src/day07.rs
+++ 2016/src/day07.rs
@@ -1,20 +1,71 @@
 use aoc_lib_rust::{Day, Example, Solution};
+use std::collections::HashSet;
 
 pub struct Day07;
 
 impl Day for Day07 {
 
-    const PART1: Solution = Solution::None;
-    const PART2: Solution = Solution::None;
+    const PART1: Solution = Solution::U32(105);
+    const PART2: Solution = Solution::U32(258);
 
-    fn solve(_input: &str) -> [Solution; 2] {
-        [Solution::None, Solution::None]
+    fn solve(input: &str) -> [Solution; 2] {
+        let sol1 = input.lines().filter(|line| {
+            let mut found = false;
+            let mut in_brackets = false;
+            for window in line.as_bytes().windows(4) {
+                if window.contains(&b'[') {
+                    in_brackets = true;
+                }
+                if window.contains(&b']') {
+                    in_brackets = false;
+                }
+                if window[0] == window[3] && window[1] == window[2] && window[0] != window[1] {
+                    if in_brackets {
+                        return false;
+                    }
+                    found = true;
+                }
+            }
+            return found;
+        }).count();
+
+        let sol2 = input.lines().filter(|line| {
+            let mut abas = HashSet::new();
+            let mut in_brackets = false;
+            for window in line.as_bytes().windows(3) {
+                if window.contains(&b'[') {
+                    in_brackets = true;
+                }
+                if window.contains(&b']') {
+                    in_brackets = false;
+                }
+                if window[0] == window[2] && window[0] != window[1] {
+                    if abas.contains(&(!in_brackets, window[1], window[0])) {
+                        return true;
+                    }
+                    abas.insert((in_brackets, window[0], window[1]));
+                }
+            }
+            return false;
+        }).count();
+
+        [Solution::U32(sol1 as _), Solution::U32(sol2 as _)]
     }
 
     const EXAMPLES: &'static [Example] = &[
         Example {
-            solution: [Solution::None, Solution::None],
-            input: "",
+            solution: [Solution::U32(2), Solution::None],
+            input: "abba[mnop]qrst
+abcd[bddb]xyyx
+aaaa[qwer]tyui
+ioxxoj[asdfgh]zxcvbn",
+        },
+        Example {
+            solution: [Solution::U32(0), Solution::U32(3)],
+            input: "aba[bab]xyz
+xyx[xyx]xyx
+aaa[kek]eke
+zazbz[bzb]cdb",
         },
     ];
 }
