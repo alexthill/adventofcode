812e5fbb9a22fe641d42dbe90d850241b610c153
alexthill <thial058@gmail.com>
2025-11-05 08:37:01 +0100

    2016 day15-16

--- 2016/src/day15.rs
+++ 2016/src/day15.rs
@@ -1,20 +1,51 @@
-use aoc_lib_rust::{Day, Example, Solution};
+use aoc_lib_rust::{next_parse, Day, Example, Solution};
 
 pub struct Day15;
 
 impl Day for Day15 {
 
-    const PART1: Solution = Solution::None;
-    const PART2: Solution = Solution::None;
+    const PART1: Solution = Solution::U32(121834);
+    const PART2: Solution = Solution::U32(3208099);
 
-    fn solve(_input: &str) -> [Solution; 2] {
-        [Solution::None, Solution::None]
+    #[allow(clippy::manual_is_multiple_of)]
+    fn solve(input: &str) -> [Solution; 2] {
+        let discs = input.lines().map(|line| {
+            let mut parts = line["Disc #1 has ".len()..line.len() - 1]
+                .split(" positions; at time=0, it is at position ");
+            (next_parse!(parts, u32), next_parse!(parts, u32))
+        }).collect::<Vec<_>>();
+
+        let mut i = 0;
+        let sol1 = 'outer: loop {
+            for (j, (count, pos)) in discs.iter().copied().enumerate() {
+                if (pos + i + j as u32 + 1) % count != 0 {
+                    i += 1;
+                    continue 'outer;
+                }
+            }
+            break i;
+        };
+
+        let period: u32 = discs.iter().map(|disc| disc.0).product();
+        let mut i = 0;
+        let sol2 = 'outer: loop {
+            let pos = sol1 + period * i;
+            if (pos + discs.len() as u32 + 1) % 11 != 0 {
+                i += 1;
+                continue 'outer;
+            }
+            break pos;
+        };
+
+        [Solution::U32(sol1), Solution::U32(sol2)]
     }
 
     const EXAMPLES: &'static [Example] = &[
         Example {
-            solution: [Solution::None, Solution::None],
-            input: "",
+            solution: [Solution::U32(5), Solution::U32(85)],
+            input: "Disc #1 has 5 positions; at time=0, it is at position 4.
+Disc #2 has 2 positions; at time=0, it is at position 1.
+",
         },
     ];
 }
--- 2016/src/day16.rs
+++ 2016/src/day16.rs
@@ -4,17 +4,44 @@ pub struct Day16;
 
 impl Day for Day16 {
 
-    const PART1: Solution = Solution::None;
-    const PART2: Solution = Solution::None;
+    const PART1: Solution = Solution::Str("10100011010101011");
+    const PART2: Solution = Solution::Str("01010001101011001");
 
-    fn solve(_input: &str) -> [Solution; 2] {
-        [Solution::None, Solution::None]
+    fn solve2(input: &str, example: bool) -> [Solution; 2] {
+        fn checksum(input: &[u8], disk: usize) -> String {
+            let mut data = input.to_vec();
+            while data.len() < disk {
+                let cpy = data.iter().copied().rev()
+                    .map(|x| x ^ 1)
+                    .collect::<Vec<_>>();
+                data.push(b'0');
+                data.extend(cpy);
+            }
+
+            data.truncate(disk);
+            while data.len().is_multiple_of(2) {
+                for i in 0..data.len() / 2 {
+                    data[i] = if data[i * 2] == data [i * 2 + 1] { b'1' } else { b'0' }
+                }
+                data.truncate(data.len() / 2);
+            }
+
+            String::from_utf8(data).unwrap()
+        }
+
+        let (sol1, sol2) = if example {
+            (checksum(input.as_bytes(), 20), String::new())
+        } else {
+            (checksum(input.as_bytes(), 272), checksum(input.as_bytes(), 35651584))
+        };
+
+        [Solution::String(sol1), Solution::String(sol2)]
     }
 
     const EXAMPLES: &'static [Example] = &[
         Example {
-            solution: [Solution::None, Solution::None],
-            input: "",
+            solution: [Solution::Str("01100"), Solution::None],
+            input: "10000",
         },
     ];
 }
