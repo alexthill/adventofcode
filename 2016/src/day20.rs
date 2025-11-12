7ca9963f449e126f7374482a607f8656d76e8085
alexthill <thial058@gmail.com>
2025-11-12 09:00:57 +0100

    2016 day20

--- 2016/src/day20.rs
+++ 2016/src/day20.rs
@@ -1,20 +1,44 @@
-use aoc_lib_rust::{Day, Example, Solution};
+use aoc_lib_rust::{next_parse, Day, Example, Solution};
 
 pub struct Day20;
 
 impl Day for Day20 {
 
-    const PART1: Solution = Solution::None;
-    const PART2: Solution = Solution::None;
+    const PART1: Solution = Solution::U32(23923783);
+    const PART2: Solution = Solution::U32(125);
 
-    fn solve(_input: &str) -> [Solution; 2] {
-        [Solution::None, Solution::None]
+    fn solve2(input: &str, example: bool) -> [Solution; 2] {
+        let max_valid = if example { 9 } else { u32::MAX };
+        let mut ranges = input.lines().map(|line| {
+            let mut parts = line.split('-');
+            next_parse!(parts, u32)..=next_parse!(parts, u32)
+        }).collect::<Vec<_>>();
+        ranges.sort_unstable_by_key(|range| *range.start());
+
+        let mut high = 0;
+        let mut sol1 = u32::MAX;
+        let mut sol2 = 0;
+        for range in ranges.iter() {
+            if *range.start() > high + 1 {
+                sol1 = sol1.min(high + 1);
+                sol2 += *range.start() - high - 1;
+            }
+            high = high.max(*range.end());
+            if high == max_valid {
+                break;
+            }
+        }
+        sol2 += max_valid - high;
+
+        [Solution::U32(sol1), Solution::U32(sol2)]
     }
 
     const EXAMPLES: &'static [Example] = &[
         Example {
-            solution: [Solution::None, Solution::None],
-            input: "",
+            solution: [Solution::U32(3), Solution::U32(2)],
+            input: "5-8
+0-2
+4-7",
         },
     ];
 }
